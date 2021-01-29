827f207a68620dabb3c9801299844e978f1a40cc
Gijs Wobben <gijswobben@gmail.com>
2021-01-29 17:06:30 +0100

    Updated copyright info

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 # -- Project information -----------------------------------------------------
 
 project = "customs"
-copyright = "2020, Gijs Wobben"
+copyright = "2021, Gijs Wobben"
 author = "Gijs Wobben"
 
 
